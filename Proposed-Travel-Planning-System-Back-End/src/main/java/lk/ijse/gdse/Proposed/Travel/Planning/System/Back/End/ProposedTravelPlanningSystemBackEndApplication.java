6dfd6e7099e8e6f5bd2d1d9ba7c3550124685565
Maleen Ashinshana <maleenashinshana70@gmail.com>
2023-10-08 14:28:44 +0530

    first commit

---
+++ Proposed-Travel-Planning-System-Back-End/src/main/java/lk/ijse/gdse/Proposed/Travel/Planning/System/Back/End/ProposedTravelPlanningSystemBackEndApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.Proposed.Travel.Planning.System.Back.End;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProposedTravelPlanningSystemBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProposedTravelPlanningSystemBackEndApplication.class, args);
+	}
+
+}
