877a61514b7223769141735383e3ce81a9e3d8ae
Maleen Ashinshana <maleenashinshana70@gmail.com>
2023-10-09 12:36:45 +0530

    create the dto classes

--- Proposed-Travel-Planning-System-Back-End/src/main/java/lk/ijse/gdse/Proposed/Travel/Planning/System/Back/End/ProposedTravelPlanningSystemBackEndApplication.java
+++ Proposed-Travel-Planning-System-Back-End/src/main/java/lk/ijse/gdse/Proposed/Travel/Planning/System/Back/End/ProposedTravelPlanningSystemBackEndApplication.java
@@ -1,13 +1,19 @@
 package lk.ijse.gdse.Proposed.Travel.Planning.System.Back.End;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProposedTravelPlanningSystemBackEndApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProposedTravelPlanningSystemBackEndApplication.class, args);
+	}
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
 	}
 
 }
---
+++ Proposed-Travel-Planning-System-Back-End/src/main/java/lk/ijse/gdse/Proposed/Travel/Planning/System/Back/End/converter/Convert.java
@@ -0,0 +1,71 @@
+package lk.ijse.gdse.Proposed.Travel.Planning.System.Back.End.converter;
+
+
+import lk.ijse.gdse.Proposed.Travel.Planning.System.Back.End.dto.*;
+import lk.ijse.gdse.Proposed.Travel.Planning.System.Back.End.entity.*;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Convert {
+    private  final ModelMapper modelMapper;
+
+    public Convert(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+    public AdminEntity toAdminEntity(AdminDTO adminDTO){
+        return modelMapper.map(adminDTO, AdminEntity.class);
+    }
+    public  AdminDTO toAdminDTO(AdminEntity adminEntity){
+        return modelMapper.map(adminEntity, AdminDTO.class);
+    }
+
+    public DriverEntity toDriverEntity(DriverDTO driverDTO){
+        return modelMapper.map(driverDTO, DriverEntity.class);
+    }
+    public  DriverDTO toDriverDTO(DriverDTO driverDTO){
+        return modelMapper.map(driverDTO, DriverDTO.class);
+    }
+
+    public GuideEntity toGuiderEntity(GuiderDTO guiderDTO){
+        return modelMapper.map(guiderDTO, GuideEntity.class);
+    }
+    public  GuiderDTO toGuiderDTO(GuideEntity guideEntity){
+        return modelMapper.map(guideEntity, GuiderDTO.class);
+    }
+
+    public HotelEntity toHotelEntity(HotelDTO hotelDTO){
+        return modelMapper.map(hotelDTO, HotelEntity.class);
+    }
+    public  HotelDTO toHotelDTO(HotelEntity hotelEntity){
+        return modelMapper.map(hotelEntity, HotelDTO.class);
+    }
+
+    public MainTravelServiceEntity toMainTravelServiceEntity(MainTravelServiceDTO travelServiceDTO){
+        return modelMapper.map(travelServiceDTO, MainTravelServiceEntity.class);
+    }
+    public   MainTravelServiceDTO toTravelServiceDTO(MainTravelServiceEntity travelServiceEntity){
+        return modelMapper.map(travelServiceEntity, MainTravelServiceDTO.class);
+    }
+
+    public PackageEntity toPackageEntity(PackageDTO packageDTO){
+        return modelMapper.map(packageDTO, PackageEntity.class);
+    }
+    public  PackageDTO toPackageDTO(PackageEntity packageEntity){
+        return modelMapper.map(packageEntity, PackageDTO.class);
+    }
+
+    public UserEntity toUserEntity(UserDTO userDTO){
+        return modelMapper.map(userDTO, UserEntity.class);
+    }
+    public  UserDTO toUserDTO(UserEntity userEntity){
+        return modelMapper.map(userEntity, UserDTO.class);
+    }
+
+    public VehicleEntity toVehicleEntity(VehicleDTO vehicleDTO){
+        return modelMapper.map(vehicleDTO, VehicleEntity.class);
+    }
+    public  VehicleDTO toVehicleDTO(VehicleEntity  vehicleEntity){
+        return modelMapper.map(vehicleEntity, VehicleDTO.class);
+    }
+}
